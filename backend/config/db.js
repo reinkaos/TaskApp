8dfaf1853a3be31f41d5bc338e03e9c64d1a9818
Rahulx18 <142908790+Rahulx18@users.noreply.github.com>
2024-07-04 23:28:30 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const uri =
-      "mongodb+srv://rahuliyer18:rahul18@cluster0.3cjjsyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+      "##";
     const conn = await mongoose.connect(uri, {});
     console.log("MongoDB connected successfully");
     return conn.connection.host;
