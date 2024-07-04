24af84bcaf2c536fb4da7ebcd4e7f2c9fc8e10ee
Rahulx18 <142908790+Rahulx18@users.noreply.github.com>
2024-07-04 23:35:22 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    const uri =
-      "##";
+    const uri = process.env.MONGO_URI;
     const conn = await mongoose.connect(uri, {});
     console.log("MongoDB connected successfully");
     return conn.connection.host;
