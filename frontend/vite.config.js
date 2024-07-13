9c140ff69d6b68bcd03b25fc5d708032a3a12a0a
reinkaos <rahuliyer18@gmail.com>
2024-07-14 00:46:56 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@reduxjs/toolkit"],
+      external: /^@reduxjs\/toolkit/,
     },
   },
 });
