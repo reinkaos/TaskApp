f413b393e4f1d3904c954a8ada4fc10185f1db7d
Rahulx18 <rahuliyer18@gmail.com>
2024-08-28 00:02:06 +0530

    c

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react", "react-dom", /^@reduxjs\/toolkit/],
+      external: [/^@reduxjs\/toolkit/], // Keep external only if necessary
     },
   },
 });
